f401b8eab0c45cd92e608f9cb705862293f5b5bc
conmeconte <saja523@hotmail.com>
2018-01-31 11:42:11 -0800

    init commit

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Guessing Game made with React
 
-> This repo contains the base setup for creating a number guessing game in react
+### Setup Instruction 
+
+> l. Guessing Game Created with ReactJS
 >
-> Setup instructions and feature sets can be found at <a href="http://reactprototypes.com/projects/guessing-game" target="_blank">http://reactprototypes.com/projects/guessing-game</a>
+>       - Setup instructions and feature sets can be found at <a href="http://reactprototypes.com/conmeconte/react_guessing-game2" target="_blank">Here</a>
--- src/components/game.js
+++ src/components/game.js
@@ -4,13 +4,43 @@ class Game extends Component {
     constructor(props){
         super(props);
 
-        this.state = {};
+        this.state = {
+            randomNumber: '',
+            userGuess: '',
+        };
+
+        this.handleInput= this.handleInput.bind(this); 
+
     }
 
+    handleInput(event){
+        const value= event.target.value;
+        const {userGuess}= this.state; 
+
+        this.setState({
+            userGuess: value
+        })
+    }
+    randomNumberCreator(){
+
+    }
+
+    reset(){
+        this.setState({
+            randomNumber: '',
+            userGuess: ''
+        });
+    }
     render(){
-        return (
+        return(
             <div>
                 <h1 className="text-center my-3">Guessing Game</h1>
+                <form>
+                    <p value={this.state.userGuess}></p>
+                    <input value={this.state.userGuess} onChange={this.handleInput}/>
+                    <button onClick={this.reset}>Reset</button>
+                    <button>Submit</button>
+                </form>
             </div>
         )
     }
